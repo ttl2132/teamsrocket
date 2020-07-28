dd927b46a4b8eaa74304fbfe7cb5556ebc91f5fb
Tian Low <t-tilow@microsoft.com>
2020-07-28 19:41:00 -0400

    Adding coffee image

--- TeamsRocketSticker-src/bot.js
+++ TeamsRocketSticker-src/bot.js
@@ -1,15 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, MessageFactory, CardFactory } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            const replyText = `${ context.activity.text }`;
+            const card = CardFactory.heroCard(
+                'Coffee',
+                ['https://github.com/ttl2132/teamsrocket/blob/master/TeamsRocketSticker-src/coffee.png']
+            )
+            await context.sendActivity(MessageFactory.attachment(card));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
