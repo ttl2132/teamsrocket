7d01610edfe849c019957fbc6e884ad39cc1a305
Tian Low <t-tilow@microsoft.com>
2020-07-29 22:51:52 -0400

    Adding more images

--- TeamsRocketSticker-src/bot.js
+++ TeamsRocketSticker-src/bot.js
@@ -8,46 +8,97 @@ class EchoBot extends ActivityHandler {
         super();
         this.onMessage(async (context, next) => {
             let test = new Map();
-            test.set(':teamrocket:', CardFactory.heroCard(
-                'Team Rocket!',
+            test.set(':teamrocket:', CardFactory.heroCard("",
                 ['https://cdn.upstation.asia/wp-content/uploads/sites/6/2019/07/27064023/5517.jpeg']
             ));
-            test.set(':test:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/dCbZtZl.jpg']
+            test.set(':sleeplisten:', CardFactory.heroCard("",
+                ['https://i.imgur.com/L5HAOhB.jpg']
             ));
-            test.set(':smiley:', CardFactory.heroCard(
-                'smiley',
-                ['https://i.imgur.com/v4vFMcb.jpg']
+            test.set(':mind:', CardFactory.heroCard("",
+                ['https://i.imgur.com/4xuXQu4.jpg']
             ));
-            test.set(':sillysmiley:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/7g2rXu8.jpg']
+            test.set(':toiletpaper:', CardFactory.heroCard("",
+                ['https://i.imgur.com/FN0c2MZ.jpg']
             ));
-            test.set(':toiletpaper:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/GS5n6M4.jpg']
-            ));
-            test.set(':coffeemountain:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/cPQAz0E.jpg']
+            test.set(':coffeemountain:', CardFactory.heroCard("",
+                ['https://i.imgur.com/tTvf0WF.jpg']
             ));
             test.set(':mounttwoply:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/kUuDfg4.jpg']
+                "",
+                ['https://i.imgur.com/6iLwBO3.jpg']
             ));
             test.set(':mountoneply:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/DCVlkwd.jpg']
+                "",
+                ['https://i.imgur.com/ZgsesZK.jpg']
             ));
+            test.set(':sleephurt:', CardFactory.heroCard(
+                "",
+                ['https://i.imgur.com/nt5oqSu.jpg']
+            ))
             test.set(':shower:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/WuyXC3i.jpg']
+                '',
+                ['https://i.imgur.com/dZpmk93.jpg']
             ));
+            test.set(':sleepwatch:', CardFactory.heroCard(
+                "", ['https://i.imgur.com/cFZsaaa.jpg']
+            ))
             test.set(':work:', CardFactory.heroCard(
-                'test',
-                ['https://i.imgur.com/DDD8yYu.jpg']
+                '',
+                ['https://i.imgur.com/QlkxNZg.jpg']
             ));
+            test.set(':redmeeting:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/pRrRKzO.jpg']
+            ));
+            test.set(':bluemeeting:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/e5Kdf2C.jpg']
+            ));
+            test.set(':growthmindset:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/P1C1iwV.jpg']
+            ));
+            test.set(':hardstop:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/gNTsWUp.jpg']
+            ));
+            test.set(':mute:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/KHocrP5.jpg']
+            ));
+            test.set(':handsan:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/7Ssa07Q.jpg']
+            ));
+            test.set(':deadteams:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/tp4Vwaq.jpg']
+            ));
+            test.set(':music:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/9xECHhA.jpg']
+            ));
+            test.set(':monday:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/6uQ0p6m.jpg']
+            ));
+            test.set(':tuesday:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/GPrazrd.jpg']
+            ));
+            test.set(':wednesday:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/j8OMXx6.jpg']
+            ));
+            test.set(':thursday:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/pn1cL3q.jpg']
+            ));
+            test.set(':friday:', CardFactory.heroCard(
+                '',
+                ['https://i.imgur.com/rmOwVtT.jpg']
+            ));
+            
             var keyword = context.activity.text.substr("Teams Rocket Stickers".length + 4, context.activity.text.length).trim();
             if (test.has(keyword))
                 await context.sendActivity(MessageFactory.attachment(test.get(keyword)));
@@ -63,7 +114,7 @@ class EchoBot extends ActivityHandler {
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Hello and welcome! Type "@teamsrocketbot2 help" for available stickers:';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
